930b52b070a5a2e14253c8efe55a0e00a8c844a6
Felix Odete <felix.odette@gmail.com>
2022-07-17 18:21:15 +0300

    Create tables, inserted data, and did queries.

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Agumon', '02/03/2020', 0, true, 10.23),
+       ('Gabumon', '11/15/2018', 2, true, 8),
+       ('Pickachu', '07/01/2021', 1, false, 15.05),
+       ('Devimon', '05/12/2017', 5, true, 11);
